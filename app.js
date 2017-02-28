0787e6507c844973a253405e02bea42f294adbba
krm22 <krmouat22@gmail.com>
2017-02-28 11:09:15 +0100

    msgs sent

--- app.js
+++ app.js
@@ -11,9 +11,16 @@ app.use(express.static(path.join(__dirname, "public")));
 
 io.on('connection', function(socket){
   console.log('new connection made');
-})
 
 
+  socket.emit('message-from-server',{
+    greeting: 'You are now being served from your favourite server 100.KM'
+  });
+
+  socket.on('message-from-client', function(msg){
+    console.log(msg);
+  });
+});
 
 server.listen(port, function(){
   console.log('Listening on port ' +  port);
